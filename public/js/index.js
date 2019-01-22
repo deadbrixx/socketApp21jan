4d110d6e8014ea0df3d10145f007b6f2b5fa4046
deadbrixx <ashidavv@gmail.com>
2019-01-23 02:20:21 +0530

    messages events are added

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+           socket.on('connect',function (){
+               console.log("Connected to server Successfully...")
+
+               //Here we are emiting custom events through socket.emit()
+               socket.emit('sendMessage', {
+                   from: 'pinkymandal',
+                   message: 'pinkymandal@gmail.com'
+               })
+           });
+           socket.on('disconnect',function (){
+               console.log("Disconnected to server...")
+           });
+
+           //Here we are listening custom event through socket.on()
+           socket.on('newMessage', function(anyparameter){
+               console.log("For recieving email from server", anyparameter);  
+           })
--- server/server.js
+++ server/server.js
@@ -2,11 +2,40 @@ var path = require('path');
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var http = require('http');
+var socketIO = require('socket.io');
 
 var publicFolder = path.join(__dirname, '../public');
 app.use(express.static(publicFolder));
 
-app.listen(port, (err) => {
+//for manual http server, we are calling http method
+var server = http.createServer(app);
+
+//for create socketio server[webscocket server]
+var io = socketIO(server);
+io.on('connection',(socket)=>{
+    console.log("New User Connected...");
+
+    //Here we are emiting custom events through socket.emit()
+    socket.emit('newMessage', {
+        name: 'Manish Bros',
+        email: 'moongerraja@gmail.com',
+        message: 'Mil kar khushi hui'
+    });
+
+    //Here we are listening custom event through socket.on()
+    socket.on('sendMessage', (anyparameter) => {
+        console.log("Data here", anyparameter);
+    })
+
+socket.on('disconnect',()=>{
+    console.log("User Disconnected!")
+})
+})
+
+
+// app.listen(port, (err) => {
+server.listen(port, (err) => {
     if(err) throw err;
     else{
         console.log(`Server listening on ${port}`);
